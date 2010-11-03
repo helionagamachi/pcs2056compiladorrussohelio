cd2153dcfa44aa08bde37e187bfd1e9e6453cebf
helionagamachi@gmail.com <helionagamachi@gmail.com@ba75546e-13f1-79f3-6992-901573fc5550>
2010-11-03 00:47:36 +0000

    Good idea on how to make the transitions, yet to be finished.....
    Next Step : Parser and The Structed Automata class

--- trunk/Compilador/src/syntatical/FiniteAutomata.java
+++ trunk/Compilador/src/syntatical/FiniteAutomata.java
@@ -11,11 +11,71 @@ package syntatical;
  */
 public class FiniteAutomata {
 
-    private FiniteAutomataState initialState;
-    private FiniteAutomataState currentState;
+    private FiniteAutomataState[] states;
+    private Transition[] transitions;
     private int automataNumber;
+    private int currentState;
 
+    /**
+     * Constructs a new Finite Automata
+     * @param stateAmount how many states will be needed to the automata?
+     * @param automataNumber the identification number of this finite automata.
+     */
+    public FiniteAutomata(int stateAmount, int transitionsAmount,int automataNumber) {
+        this.automataNumber = automataNumber;
+        this.transitions = new Transition[transitionsAmount];
+        this.states = new FiniteAutomataState[stateAmount];
+    }
 
-    
+    /**
+     * Initializes the list of states on the Automata, the
+     * array indicates which states are final.
+     * @param finalStates An array containing the indexes of the
+     * final states for this automata, note that it should be
+     * an ordered array in order to work
+     */
+    public void initStates(int[] finalStates){
+        this.currentState = 0;
+        int statesAmount = this.states.length;
+        int done = 0;
+        int finalStatesIndex =0 ;
+        boolean hasFinalStates;
+        hasFinalStates = finalStates.length > 0;
+        while(done < statesAmount){
+            FiniteAutomataState newState;
+            boolean isFinalState = false;
+            if(hasFinalStates){
+                if(finalStates[finalStatesIndex] == done){
+                    isFinalState = true;
+                    finalStatesIndex = finalStatesIndex + 1;
+                    if(finalStatesIndex == finalStates.length){
+                        hasFinalStates = false;
+                    }
+                }
+            }
+            newState = new FiniteAutomataState(isFinalState, done);
+            this.states[done] = newState;
+            done = done + 1;
+        }
+    }
+
+    public int getAutomataNumber() {
+        return automataNumber;
+    }
+
+    public int getCurrentState() {
+        return currentState;
+    }
+
+    public void setCurrentState(int currentState) {
+        this.currentState = currentState;
+    }
+
+    /**
+     * @return the automata is on a final state or note.
+     */
+    public boolean onFinalState(){
+        return this.states[this.currentState].isFinalState();
+    }
    
 }
