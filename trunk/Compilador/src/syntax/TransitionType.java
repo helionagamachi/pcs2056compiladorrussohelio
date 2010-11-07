a356a5c81a8f10678fc06c77392a12ca6706e698
helionagamachi@gmail.com <helionagamachi@gmail.com@ba75546e-13f1-79f3-6992-901573fc5550>
2010-11-07 21:20:07 +0000

    Before doing the config parser

--- trunk/Compilador/src/syntax/TransitionType.java
+++ trunk/Compilador/src/syntax/TransitionType.java
@@ -9,6 +9,10 @@ package syntax;
  * or it will result on a call to another automata
  */
 public enum TransitionType {
-
-    NORMAL, CALL_TO_ANOTHER_AUTOMATA;
+    //normal transtion, the finite automata, changed it's state.
+    NORMAL,
+    //Transition that requires the call of another automata.
+    CALL_TO_ANOTHER_AUTOMATA,
+    //The automata has nothing do anymore, should return.
+    GO_BACK;
 }
